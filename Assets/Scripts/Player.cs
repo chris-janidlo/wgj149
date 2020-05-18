fe1efceae7aeabb3dd5db297c6e5a267c651f126
crassSandwich <christopherjanidlo@gmail.com>
2020-05-17 23:32:43 -0400

    Separate out gliding and manual stuff

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -9,65 +9,50 @@ public class Player : MonoBehaviour
 {
     [Header("Physics")]
     public float Gravity;
+    public float DragCoefficient;
 
-    public AnimationCurve TurnSpeedByTranslationalSpeed;
+    public float HalfHeight, GroundedFudge;
+    public LayerMask GroundLayers;
 
     public float DeathCollisionSpeed;
 
     [Range(0, 1)]
     public float CriticalVerticality;
 
-    public float FlapTime, FlapBurst, FlapHorizontalScale;
+    public float FreefallBrakeAngle;
 
+    public AnimationCurve TurnSpeedByTranslationalSpeed;
     public Vector2 PitchRange = new Vector2(-90, 90);
 
-    public float DragCoefficientWhenFlapping, DragCoefficientWhenGliding;
-    public float MaximumFallSpeedWhenBraking;
-
-    public float HalfHeight, GroundedFudge;
-    public LayerMask GroundLayers;
+    [Tooltip("X is forward/backward, Y is side to side")]
+    public Vector2 ManualFlyingAcceleration;
+    public float ManualFlyingMaxSpeed, ManualFlyingDeceleration;
 
     [Header("Resources")]
     public float MaxStamina;
-
-    public float StaminaChangePerSecondFlying, StaminaChangePerSecondResting, FlapStaminaCost;
-    public float StaminaStunPeriod;
-
-    public int BurstSlots;
-    public float BurstStunPeriod;
-    public AnimationCurve BurstRecoveryPerSecondByAvailableBurst; // where each slot is worth 100
-
-    public float FallSlowAdditionalStaminaCostPerSecond;
+    public float StaminaChangePerSecondManualFlying, StaminaChangePerSecondResting;
 
     [Header("Controls")]
-    public string FlapButton;
-    public string TranslationalAxis, PitchAxis, YawAxis;
     public bool InversePitch;
+    public string ForwardAxis, SidewaysAxis, PitchAxis, YawAxis;
 
     [Header("References")]
     public Rigidbody Rigidbody;
-
-    public UnityEvent TriedToFlapWhenUnableTo, Died;
+    public UnityEvent Died;
 
     public float Stamina { get; private set; }
-    public float BurstStamina { get; private set; }
 
-    bool flapInput { get; set; }
-    // scalar for z force on flap (ie you move forward a little on flap when this is positive, or back a little when negative)
-    float translationalInput { get; set; }
+    // x is forward/backward, y is side to side
+    Vector2 translationalInput;
     // x is yaw, y is pitch
-    Vector2 rotationalInput { get; set; }
+    Vector2 rotationalInput;
 
+    public Vector3 automaticVelocityState, manualVelocityState;
     Vector2 targetRotation;
-
-    float flapTimer;
-
-    float staminaStunTimer, burstStaminaStunTimer;
 
     void Start ()
     {
         Stamina = MaxStamina;
-        BurstStamina = BurstSlots * 100;
     }
 
     void Update ()
@@ -98,35 +83,32 @@ public class Player : MonoBehaviour
 
     void trackInput ()
     {
-        flapInput = Input.GetButton(FlapButton);
-        translationalInput = Input.GetAxis(TranslationalAxis);
+        translationalInput = new Vector2
+        (
+            Input.GetAxis(ForwardAxis),
+            Input.GetAxis(SidewaysAxis)
+        );
+
         rotationalInput = new Vector2
         (
             Input.GetAxis(YawAxis),
             Input.GetAxis(PitchAxis) * (InversePitch ? -1 : 1)
         );
-
-        flapTimer -= Time.deltaTime;
     }
 
     void manageStamina ()
     {
-        staminaStunTimer -= Time.deltaTime;
-        if (staminaStunTimer <= 0)
-        {
-            bool touchingGround = Physics.SphereCast(new Ray(transform.position, Vector3.down), GroundedFudge, HalfHeight, GroundLayers);
-            bool resting = touchingGround && Rigidbody.velocity.sqrMagnitude == 0;
+        bool touchingGround = Physics.SphereCast(new Ray(transform.position, Vector3.down), GroundedFudge, HalfHeight, GroundLayers);
+        bool resting = touchingGround && Rigidbody.velocity.sqrMagnitude == 0;
 
-            Stamina += (resting ? StaminaChangePerSecondResting : StaminaChangePerSecondFlying) * Time.deltaTime;
-            Stamina = Mathf.Clamp(Stamina, 0, MaxStamina);
-        }
+        bool flying = translationalInput != Vector2.zero;
 
-        burstStaminaStunTimer -= Time.deltaTime;
-        if (burstStaminaStunTimer <= 0)
-        {
-            BurstStamina += BurstRecoveryPerSecondByAvailableBurst.Evaluate(BurstStamina) * Time.deltaTime;
-            BurstStamina = Mathf.Min(BurstStamina, BurstSlots * 100);
-        }
+        float stamChange = resting ? StaminaChangePerSecondResting
+                         : flying ? StaminaChangePerSecondManualFlying
+                         : 0;
+
+        Stamina += stamChange * Time.deltaTime;
+        Stamina = Mathf.Clamp(Stamina, 0, MaxStamina);
     }
 
     void rotate ()
@@ -144,75 +126,52 @@ public class Player : MonoBehaviour
 
     void translate ()
     {
-        Rigidbody.AddForce(Vector3.down * Gravity, ForceMode.Acceleration);
+        // AUTOMATIC
 
-        if (Stamina <= 0) return;
+        automaticVelocityState += Vector3.down * Gravity * Time.deltaTime;
 
-        bool flapping = flapTimer > 0;
+        if (Stamina > 0) glide();
 
-        if (!flapping)
+        automaticVelocityState += -DragCoefficient * automaticVelocityState.normalized * automaticVelocityState.sqrMagnitude * Time.deltaTime;
+
+        // MANUAL
+
+        Vector3 manualVelocityChange;
+
+        bool flyingManually = Stamina > 0 && translationalInput != Vector2.zero;
+
+        if (!flyingManually)
         {
-            bool flappable = canFlap();
-
-            if (!flapInput || !flappable)
-            {
-                glide();
-            }
-            else if (flapInput && !flappable)
-            {
-                TriedToFlapWhenUnableTo.Invoke();
-            }
-            else
-            {
-                flapping = true;
-                flap();
-            }
+            manualVelocityChange = -manualVelocityState.normalized * ManualFlyingDeceleration;
+            manualVelocityChange = Vector3.ClampMagnitude(manualVelocityChange, manualVelocityState.magnitude);
         }
-
-        float drag = flapping ? DragCoefficientWhenFlapping : DragCoefficientWhenGliding;
-        Rigidbody.AddForce(-drag * Rigidbody.velocity.normalized * Rigidbody.velocity.sqrMagnitude, ForceMode.Acceleration);
-
-        if (flapping)
+        else
         {
-            float staminaCostThisFrame = FallSlowAdditionalStaminaCostPerSecond * Time.deltaTime;
-
-            // if we're braking and falling and have stamina
-            if (translationalInput < 0 && Rigidbody.velocity.y < 0 && -Rigidbody.velocity.y > Mathf.Abs(Rigidbody.velocity.x) && -Rigidbody.velocity.y > Mathf.Abs(Rigidbody.velocity.z) && Stamina > staminaCostThisFrame)
-            {
-                Rigidbody.velocity = Vector3.ClampMagnitude(Rigidbody.velocity, MaximumFallSpeedWhenBraking);
-                Stamina -= staminaCostThisFrame;
-            }
-        }
-    }
-
-    bool canFlap ()
-    {
-        return translationalInput < 0 || (Stamina >= FlapStaminaCost && BurstStamina >= 100);
-    }
-
-    void flap ()
-    {
-        flapTimer = FlapTime;
-
-        var force = (Vector3.up + translationalInput * transform.forward * FlapHorizontalScale).normalized * FlapBurst;
-
-        if (translationalInput < 0)
-        {
-            Vector2 ignoringGravity = new Vector2
+            Vector2 direction = translationalInput.normalized;
+            
+            manualVelocityChange = new Vector3
             (
-                Rigidbody.velocity.x,
-                Rigidbody.velocity.z
+                direction.y * ManualFlyingAcceleration.y,
+                0,
+                direction.x * ManualFlyingAcceleration.x
             );
-
-            force = Vector3.ClampMagnitude(force, ignoringGravity.magnitude);
         }
 
-        Rigidbody.AddForce(force, ForceMode.VelocityChange);
+        manualVelocityState += manualVelocityChange * Time.deltaTime;
+        manualVelocityState = Vector3.ClampMagnitude(manualVelocityState, ManualFlyingMaxSpeed);
 
-        if (translationalInput >= 0)
+        // COMBINE
+
+        if (flyingManually && targetRotation.y >= FreefallBrakeAngle)
         {
-            consumeStamina(FlapStaminaCost);
-            consumeBurstSlot();
+            automaticVelocityState = Vector3.ClampMagnitude(automaticVelocityState, ManualFlyingMaxSpeed);
+        }
+
+        Rigidbody.velocity = automaticVelocityState + transform.TransformDirection(manualVelocityState);
+
+        if (flyingManually && targetRotation.y >= FreefallBrakeAngle)
+        {
+            Rigidbody.velocity = Vector3.ClampMagnitude(Rigidbody.velocity, ManualFlyingMaxSpeed);
         }
     }
 
@@ -227,21 +186,9 @@ public class Player : MonoBehaviour
             (1 - verticality) * transform.forward.z
         );
 
-        if (verticality >= CriticalVerticality && Mathf.Sign(moveDirection.y) != Mathf.Sign(Rigidbody.velocity.y))
+        if (verticality >= CriticalVerticality && Mathf.Sign(moveDirection.y) != Mathf.Sign(automaticVelocityState.y))
             moveDirection.y *= -1;
 
-        Rigidbody.velocity = moveDirection.normalized * Rigidbody.velocity.magnitude;
-    }
-
-    void consumeStamina (float amount)
-    {
-        Stamina -= amount;
-        staminaStunTimer = StaminaStunPeriod;
-    }
-
-    void consumeBurstSlot ()
-    {
-        BurstStamina -= 100;
-        burstStaminaStunTimer = BurstStunPeriod;
+        automaticVelocityState = moveDirection.normalized * automaticVelocityState.magnitude;
     }
 }
